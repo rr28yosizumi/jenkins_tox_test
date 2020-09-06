64ef1f6f2d4c047f02b03672203e5fe2ef9d382e
t_watanabe <t_watanabe@incrementp.co.jp>
2020-09-06 18:43:48 +0900

    unittest demo code

--- jenkins_tox_test/jenkins_tox_test.py
+++ jenkins_tox_test/jenkins_tox_test.py
@@ -1,1 +1,13 @@
 """Main module."""
+
+def realnum_format(x,n=8):
+    """
+    xの文字列で指定した数値を少数点n桁目まで0埋めした文字列を返す
+    ただし、xが小数点n+1桁以上あれば、n桁目以降は切り捨てる
+    """
+    return ('{:.'+str(n)+'f}').format(
+        float(
+            # 少数点がなければ、そのまま。小数点があれば、少数n桁までをスライスする
+            x[:min(x.find('.')+1+n,len(x))] if '.' in x else x
+        )
+    )
--- tests/test_jenkins_tox_test.py
+++ tests/test_jenkins_tox_test.py
@@ -19,3 +19,53 @@ class TestJenkins_tox_test(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
+        testlist = (
+            '1.0',
+            '123.0',
+            '200',
+            '10',
+            '0',
+            '0.12345678',
+            '0.123456789',
+            '123.123456789',
+            '1234.123456789999999'
+            )
+        resultlist = (
+            '1.00000000',
+            '123.00000000',
+            '200.00000000',
+            '10.00000000',
+            '0.00000000',
+            '0.12345678',
+            '0.12345678',
+            '123.12345678',
+            '1234.12345678'
+        )
+        result = [jenkins_tox_test.realnum_format(i) for i in testlist]
+
+        self.assertListEqual(result,list(resultlist))
+
+        testlist =(
+            '0.1',
+            '0.123',
+            '0.1234',
+            '0.12345',
+            '0.123456',
+            '0.1234567',
+            '0.12345678',
+            '0.123456789',
+        )
+        resultlist =(
+            '0.10',
+            '0.12',
+            '0.12',
+            '0.12',
+            '0.12',
+            '0.12',
+            '0.12',
+            '0.12',
+        )
+        result = [jenkins_tox_test.realnum_format(i,2) for i in testlist]
+
+        self.assertListEqual(result,list(resultlist))
+
